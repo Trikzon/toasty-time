5d974320adfbbbfc7acfbf3d1c8b8bd512d91324
TimeAlchemist <96148194+TimeAlchemist@users.noreply.github.com>
2024-03-30 19:46:44 -0400

    Rotation cooking + rudimentary scoring

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -5,15 +5,16 @@ extends Node3D
 @export var MAX_STICK_DISTANCE: float = 0.35
 @export var ROTATION_SPEED: float = 0.01
 @export var COOK_SPEED: float = 1.0
+@export var COOK_TOL: float = 0.1
 
 @onready var stick: Node3D = $Stick
 @onready var original_pos: Vector3 = stick.global_position
-@onready var marshmallow_cook
 @onready var marshmallow_cook_time
 
+var marshmallow_cook_values: Array[float] = []
+var most_recent_score=0
 var stick_distance = 0
 var rotation_position = 0
-
 var selected: bool = false:
 	set(value):
 		$Camera3D.current = value
@@ -21,19 +22,46 @@ var selected: bool = false:
 
 func _ready():
 	$Body.visible = false
+	
+	for _i in range(360):
+		marshmallow_cook_values.append(0.0)
+	
 	prep_marshmallow()
 
 func _physics_process(delta):
 	if selected:
 		input()
 	if(stick_distance>0.2):
-		marshmallow_cook+=delta*COOK_SPEED*stick_distance
-	if marshmallow_cook>=marshmallow_cook_time&&stick_distance<=0.2:
+		for i in range(360):
+			marshmallow_cook_values[i]+=(abs(180-(abs(i-(rad_to_deg(rotation_position) as int))))/180.0)*delta*COOK_SPEED*stick_distance
+	var cooked_count = 0
+	if stick_distance <= 0.2:
+		for i in range(360):
+			cooked_count+=int(marshmallow_cook_values[i] >= marshmallow_cook_time)
+	
+	#debug
+	for i in range(360):
+		if i%30==0&&selected:
+			print(marshmallow_cook_values[i])
+	
+	if (cooked_count/360.0)>=(1.0-COOK_TOL):
 		prep_marshmallow()
-	print(marshmallow_cook)
 	
 func prep_marshmallow():
-	marshmallow_cook=0.0
+	var mean_cook=0
+	for i in range(360):
+		mean_cook+=marshmallow_cook_values[i]
+	mean_cook/=360
+	var temp_sum=0
+	for i in range(360):
+		temp_sum+=pow(marshmallow_cook_values[i]-mean_cook,2)
+	temp_sum/=360
+	#most_recent_score
+	#sqrt(temp_sum)
+	#abs(marshmallow_cook_time-mean_cook)
+	marshmallow_cook_values.clear()
+	for _i in range(360):
+		marshmallow_cook_values.append(0)
 	marshmallow_cook_time=randf_range(10,15)
 	#add animation for marshmallow swap
 func input():
