0be87f942ac3ffebf5291b89ff4b9465e04e7eb9
TimeAlchemist <96148194+TimeAlchemist@users.noreply.github.com>
2024-03-30 15:43:12 -0400

    Add stick movement and rotation

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,7 +1,23 @@
 extends Node3D
 
-@onready var stick: Node3D = $Stick
+@export var FORWARD_SPEED: float = 0.01
+@export var MAX_STICK_DISTANCE: float = 0.5
+@export var ROTATION_SPEED: float = 0.01
 
+@onready var stick: Node3D = $Stick
+@onready var original_pos: Vector3 = stick.global_position
+
+var stick_distance = 0
+var rotation_position = 0
 
 func _process(delta):
-	pass
+	var forward_direction = Input.get_axis("move_marshmallow_back", "move_marshmallow_forward")
+	var rotation_direction = Input.get_axis("rotate_marshmallow_clockwise", "rotate_marshmallow_counterclockwise")
+	
+	stick_distance += forward_direction * FORWARD_SPEED
+	stick_distance = clamp(stick_distance, 0.0, 1.0)
+	stick.global_position = stick_distance * MAX_STICK_DISTANCE * -stick.global_transform.basis.z.normalized() + original_pos
+	
+	rotation_position += rotation_direction * ROTATION_SPEED
+	rotation_position = fposmod(rotation_position, TAU)
+	stick.rotation.z = rotation_position
